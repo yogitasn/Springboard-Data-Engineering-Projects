760c0868405aedd8446538e35f0836acfdfd5cd5
Yogita Nesargi <yogitasn@yahoo.com>
2020-11-21 20:52:47 -0500

    Added ER Diagram for Medical and Craiglist DB

--- Data Modelling Exercise/craiglist.sql
+++ Data Modelling Exercise/craiglist.sql
@@ -1,3 +1,4 @@
+drop database if exists craiglist;
 create database if not exists craiglist;
 use craiglist;
 
@@ -7,16 +8,24 @@ first_name VARCHAR(20),
 last_name VARCHAR(20),
 preferred_region VARCHAR(25));
 
+CREATE TABLE IF NOT EXISTS CATEGORY(
+category_id INT PRIMARY KEY NOT NULL,
+category VARCHAR(40)
+);
+
+CREATE TABLE IF NOT EXISTS region(
+region_id INT PRIMARY KEY NOT NULL,
+region VARCHAR(30));
+
 CREATE TABLE IF NOT EXISTS POSTS
 (postId INT PRIMARY KEY NOT NULL,
 title VARCHAR(25),
+text VARCHAR(100),
 userID INT,
 location VARCHAR(30),
-region VARCHAR(30),
-category VARCHAR(40),
-FOREIGN KEY (userID) REFERENCES USER(userID));
-
-INSERT INTO USER VALUES(1,'John','Smith','Toronto');
-
-INSERT INTO POSTS VALUES(1,'Selling Airpods',1,'Toronto','Ontario','Electronics');
+region_id INT,
+category_id INT,
+FOREIGN KEY (userID) REFERENCES USER(userID),
+FOREIGN KEY(region_id) REFERENCES region(region_id),
+FOREIGN KEY(category_id) REFERENCES category(category_id));
 
