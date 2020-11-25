b9723445ef2c127d74135fac0c1ac77fc9187c0c
Yogita Nesargi <yogitasn@yahoo.com>
2020-11-24 23:49:05 -0500

    Modified the schema for music and outer_space DB

--- Data Modelling Exercise/Schema Critique/music.sql
+++ Data Modelling Exercise/Schema Critique/music.sql
@@ -7,27 +7,131 @@ CREATE DATABASE music;
 
 USE music;
 
-CREATE TABLE songs
-(
-  id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  duration_in_seconds INTEGER NOT NULL,
-  release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+CREATE TABLE artists
+( 
+    artist_id SERIAL PRIMARY KEY,
+	name VARCHAR(100)
 );
 
-INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+CREATE TABLE producers
+(
+    producer_id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE songs
+(
+  song_id SERIAL PRIMARY KEY,
+  title VARCHAR(50),
+  duration_in_seconds INT NOT NULL,
+  release_date VARCHAR(20)
+);
+
+
+CREATE TABLE album
+(   
+	album_id SERIAL PRIMARY KEY,
+    album_name VARCHAR(50),
+    song_id BIGINT UNSIGNED,
+    producer_id BIGINT UNSIGNED,
+    FOREIGN KEY(song_id) REFERENCES songs(song_id),
+    FOREIGN KEY(producer_id) REFERENCES producers(producer_id)
+    
+);
+
+
+
+INSERT INTO artists
+	(name)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+('Hanson'),
+('Queen'),
+('Mariah Cary'),
+('Boyz II Men'),
+('Lady Gaga'),
+('Bradley Cooper'),
+('Nickelback'),
+('Jay Z'),
+('Alicia Keys'),
+('Katy Perry'),
+('Juicy J'),
+('Maroon 5'),
+('Christina Aguilera'),
+('Avril Lavigne'),
+('Destiny''s Child');
+
+INSERT INTO producers
+    (name)
+VALUES
+('Dust Brothers'),
+('Stephen Lironi'),
+('Roy Thomas Baker'),
+('Walter Afanasieff'),
+('Benjamin Rice'),
+('Rick Parashar'),
+('Al Shux'),
+('Max Martin'),
+('Cirkut'),
+('Shellback'),
+('Benny Blanco'),
+('The Matrix'),
+('Darkchild');
+
+/*INSERT INTO songs
+  (title, duration_in_seconds, release_date, artist_id)
+VALUES
+  ('MMMBop', 238, '04-15-1997', ((SELECT artist_id from artists where name='Hanson'))),
+  ('Bohemian Rhapsody', 355, '10-31-1975', ((SELECT artist_id from artists where name='Queen'))),
+  ('One Sweet Day', 282, '11-14-1995', ((SELECT artist_id from artists where name='Mariah Cary'))),
+  ('One Sweet Day', 282, '11-14-1995', ((SELECT artist_id from artists where name='Boyz II Men'))),
+  ('Shallow', 216, '09-27-2018', ((SELECT artist_id from artists where name='Lady Gaga'))),
+  ('Shallow', 216, '09-27-2018', ((SELECT artist_id from artists where name='Bradley Cooper'))),
+  ('How You Remind Me', 223, '08-21-2001', ((SELECT artist_id from artists where name='Nickelback'))),
+  ('New York State of Mind', 276, '10-20-2009', ((SELECT artist_id from artists where name='Jay Z'))),
+  ('New York State of Mind', 276, '10-20-2009', ((SELECT artist_id from artists where name='Alicia Keys'))),
+  ('Dark Horse', 215, '12-17-2013', ((SELECT artist_id from artists where name='Katy Perry'))),
+  ('Dark Horse', 215, '12-17-2013', ((SELECT artist_id from artists where name='Juicy J'))),
+  ('Moves Like Jagger', 201, '06-21-2011', ((SELECT artist_id from artists where name='Maroon 5'))),
+  ('Moves Like Jagger', 201, '06-21-2011', ((SELECT artist_id from artists where name='Christina Aguilera'))),
+  ('Complicated', 244, '05-14-2002', ((SELECT artist_id from artists where name='Avril Lavigne'))),
+  ('Say My Name', 240, '11-07-1999', ((SELECT artist_id from artists where name='Destiny''s Child')));
+*/
+
+INSERT INTO songs
+  (title, duration_in_seconds, release_date)
+VALUES
+  ('MMMBop', 238, '04-15-1997'),
+  ('Bohemian Rhapsody', 355, '10-31-1975'),
+  ('One Sweet Day', 282, '11-14-1995'),
+  ('Shallow', 216, '09-27-2018'),
+  ('How You Remind Me', 223, '08-21-2001'),
+  ('New York State of Mind', 276, '10-20-2009'),
+  ('Dark Horse', 215, '12-17-2013'),
+  ('Moves Like Jagger', 201, '06-21-2011'),
+  ('Complicated', 244, '05-14-2002'),
+  ('Say My Name', 240, '11-07-1999');
+
+INSERT INTO album
+    (album_name,song_id,producer_id)
+VALUES
+('Middle of Nowhere',((SELECT song_id from songs where title='MMMBop')),((SELECT producer_id from producers where name='Dust Brothers'))),
+('Middle of Nowhere',((SELECT song_id from songs where title='MMMBop')),((SELECT producer_id from producers where name='Stephen Lironi'))),
+('A Night at the Opera',((SELECT song_id from songs where title='Queen')),((SELECT producer_id from producers where name='Roy Thomas Baker'))),
+('Daydream',((SELECT song_id from songs where title='One Sweet Day')),((SELECT producer_id from producers where name='Walter Afanasieff'))),
+('A Star Is Born',((SELECT song_id from songs where title='Shallow')),((SELECT producer_id from producers where name='Benjamin Rice'))),
+('Silver Side Up',((SELECT song_id from songs where title='How You Remind Me')),((SELECT producer_id from producers where name='Rick Parashar'))),
+('The Blueprint 3',((SELECT song_id from songs where title='New York State of Mind')),((SELECT producer_id from producers where name='Al Shux'))),
+('Prism',((SELECT song_id from songs where title='Dark Horse')),((SELECT producer_id from producers where name='Max Martin'))),
+('Prism',((SELECT song_id from songs where title='Dark Horse')),((SELECT producer_id from producers where name='Cirkut'))),
+('Hands All Over',((SELECT song_id from songs where title='Moves Like Jagger')),((SELECT producer_id from producers where name='Shellback'))),
+('Hands All Over',((SELECT song_id from songs where title='Moves Like Jagger')),((SELECT producer_id from producers where name='Benny Blanco'))),
+('Let Go',((SELECT song_id from songs where title='Complicated')),((SELECT producer_id from producers where name='The Matrix'))),
+('The Writing''s on the Wall',((SELECT song_id from songs where title='Say My Name')),((SELECT producer_id from producers where name='Darkchild')));
+
+
+
+select * from artists;
+select * from songs;
+select * from album;
+select * from producers;
+
--- Data Modelling Exercise/Schema Critique/outer_space.sql
+++ Data Modelling Exercise/Schema Critique/outer_space.sql
@@ -6,22 +6,73 @@ DROP DATABASE IF EXISTS outer_space;
 CREATE DATABASE outer_space;
 use outer_space;
 
+CREATE TABLE moons
+( 
+   moon_id SERIAL PRIMARY KEY,
+   name VARCHAR(100)
+);
+
 CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
+  name VARCHAR(50),
   orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  orbits_around VARCHAR(50),
+  galaxy VARCHAR(10),
+  moon_id BIGINT UNSIGNED,
+  FOREIGN KEY(moon_id) REFERENCES moons(moon_id)
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+INSERT INTO moons
+    (name)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+    ('The Moon'),
+    ('Phobos'),
+    ('Deimos'),
+    ('Naiad'),
+    ('Thalassa'),
+    ('Despina'),
+    ('Galatea'),
+    ('Larissa'),
+    ('S/2004 N 1'),
+    ('Proteus'),
+    ('Triton'),
+    ('Nereid'),
+    ('Halimede'),
+    ('Sao'),
+    ('Laomedeia'),
+    ('Psamathe');
+    
+
+INSERT INTO planets
+  (name, orbital_period_in_years, orbits_around, galaxy, moon_id)
+VALUES
+  ('Earth', 1.00, 'The Sun', 'Milky Way', (select moon_id from moons where name='The Moon')),
+  ('Mars', 1.88, 'The Sun', 'Milky Way', (select moon_id from moons where name='Phobos')),
+  ('Mars', 1.88, 'The Sun', 'Milky Way', (select moon_id from moons where name='Deimos')),
+  ('Venus', 0.62, 'The Sun', 'Milky Way', NULL),
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Naiad')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Thalassa')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Despina')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Galatea')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Larissa')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='S/2004 N 1')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Proteus')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Triton')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Nereid')),
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Halimede')),
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Sao')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Laomedeia')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Psamathe')), 
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', (select moon_id from moons where name='Neso')),
+  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', NULL),
+  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', NULL);
+
+select * from planets;
+select * from moons;
+
+select m.name
+from moons m
+JOIN planets p
+ON m.moon_id=p.moon_id
+where p.name='Earth'
